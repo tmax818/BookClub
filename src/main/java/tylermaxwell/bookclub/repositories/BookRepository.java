e2377d178d7e9b2979e625c22168b45f1f0bbe85
tmax818 <tylermaxwell661@gmail.com>
2022-10-09 15:07:54 -0700

    remove solution

--- src/main/java/tylermaxwell/bookclub/repositories/BookRepository.java
+++ src/main/java/tylermaxwell/bookclub/repositories/BookRepository.java
@@ -1,10 +1,6 @@
 package tylermaxwell.bookclub.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import tylermaxwell.bookclub.models.Book;
 
-import java.util.List;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
-    List<Book> findAll();
-}
+public interface BookRepository{}
+
--- src/main/java/tylermaxwell/bookclub/services/BookService.java
+++ src/main/java/tylermaxwell/bookclub/services/BookService.java
@@ -1,42 +1,7 @@
 package tylermaxwell.bookclub.services;
 
-import org.springframework.stereotype.Service;
-import tylermaxwell.bookclub.models.Book;
-import tylermaxwell.bookclub.models.User;
-import tylermaxwell.bookclub.repositories.BookRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-@Service
 public class BookService {
 
-    private final BookRepository repo;
 
-    public BookService(BookRepository repo) {
-        this.repo = repo;
-    }
-
-    public List<Book> allBooks(){
-        return repo.findAll();
-    }
-
-    public void createBook(Book book, User user) {
-        book.setUser(user);
-        repo.save(book);
-    }
-
-    public Book findBook(Long id) {
-        Optional<Book> optionalBook = repo.findById(id);
-        return optionalBook.orElse(null);
-    }
-
-    public void updateBook(Book book, User user) {
-        book.setUser(user);
-        repo.save(book);
-    }
-
-    public void destroy(Book book){
-        repo.delete(book);
-    }
 }
