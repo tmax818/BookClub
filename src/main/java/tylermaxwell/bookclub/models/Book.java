5e4a2b66cb6f170fcf21c57c6d40fefbc1c0d869
tmax818 <tylermaxwell661@gmail.com>
2022-10-10 13:48:03 -0700

    windows update

--- src/main/java/tylermaxwell/bookclub/models/Book.java
+++ src/main/java/tylermaxwell/bookclub/models/Book.java
@@ -1,8 +1,18 @@
 package tylermaxwell.bookclub.models;
 
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
 
 }
